84f33c2cd45e85d1820fbb85f2a5f4b6af474262
Leonardo.Selmini <selminileonardo@gmail.com>
2022-02-02 18:37:25 +0100

    Footer

--- resources/views/macro/footer.blade.php
+++ resources/views/macro/footer.blade.php
@@ -1,3 +1,97 @@
 <footer>
-  
+  <section id="store" class="flex-row">
+    <div class="width-80 flex-row">
+      <div class="flex-row">
+        <img src="{{asset("/img/buy-comics-digital-comics.png")}}" alt="digital comics icon">
+        <span>digital comics</span>
+      </div>
+      <div class="flex-row">
+        <img src="{{asset("img/buy-comics-merchandise.png")}}" alt="dc merchandise icon">
+        <span>dc merchandise</span>
+      </div>
+      <div class="flex-row">
+        <img src="{{asset("img/buy-comics-subscriptions.png")}}" alt="subsciption icon">
+        <span>subscriptions</span>
+      </div>
+      <div class="flex-row">
+        <img src="{{asset("img/buy-comics-shop-locator.png")}}" alt="shop locator icon">
+        <span>shop locator</span>
+      </div>
+      <div class="flex-row">
+        <img src="{{asset("img/buy-dc-power-visa.svg")}}" alt="dc power visa icon">
+        <span>dc power visa</span>
+      </div>
+    </div>
+  </section>
+  <section id="links">
+    <div class="width-80 space-between">
+      <div class="flex">
+        <div class="column">
+          <ul>
+            <li>
+              <h2>dc comics</h2>
+            </li>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+          </ul>
+          <ul>
+            <li>
+              <h2>shop</h2>
+            </li>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+          </ul>
+        </div>
+        <div class="column">
+          <ul>
+            <li>
+              <h2>dc</h2>
+            </li>
+            <li>Term Of Use</li>
+            <li>Privacy Policy (New)</li>
+            <li>Ad Choices</li>
+            <li>Advertising</li>
+            <li>Jobs</li>
+            <li>Subscriptions</li>
+            <li>Talent Workshops</li>
+            <li>CPSC Certificates</li>
+            <li>Ratings</li>
+            <li>Shop Help</li>
+            <li>Concact Us</li>
+          </ul>
+        </div>
+        <div class="column">
+          <ul>
+            <li>
+              <h2>sites</h2>
+            </li>
+            <li>DC</li>
+            <li>MAD Magazine</li>
+            <li>DC Kids</li>
+            <li>DC Universe</li>
+            <li>DC Power Visa</li>
+          </ul>
+        </div>
+      </div>
+      <img src="{{asset("/img/dc-logo-bg.png")}}" alt="dc logo footer">
+    </div>
+  </section>
+  <section id="social">
+    <div class="width-80 space-between">
+      <a href="#">SIGN UP NOW</a>
+      <div class="social-container flex-row">
+        <span>follow us</span>
+        <img src="{{asset("/img/footer-facebook.png")}}" alt="facebook logo">
+        <img src="{{asset("/img/footer-twitter.png")}}" alt="twitter logo">
+        <img src="{{asset("/img/footer-youtube.png")}}" alt="youtube logo">
+        <img src="{{asset("/img/footer-pinterest.png")}}" alt="pinterest logo">
+        <img src="{{asset("/img/footer-periscope.png")}}" alt="periscope logo">
+      </div>
+    </div>
+  </section>
 </footer>
