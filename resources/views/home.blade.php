84d261457d0543b8fcc6a4f64a96af567b5827c4
Leonardo.Selmini <selminileonardo@gmail.com>
2022-02-02 18:08:47 +0100

    Main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,22 @@ DC | Welcome to DC
 @endsection
 
 @section('mainContent')
-
+<main>
+  <section id="jumbo">
+    <img src="{{asset("img/jumbotron.jpg")}}" alt="jumbo img">
+  </section>
+  <section id="series">
+    <div class="cs-container">
+      <h2>CURRENT SERIES</h2>
+    </div>
+    <div class="flex">
+      @foreach ($comics as $comic)  
+        <div class="card">
+          <img src="{{$comic["thumb"]}}" alt="{{$comic["title"]}}">
+          <h4>{{$comic["title"]}}</h4>
+        </div>
+      @endforeach
+    </div>
+  </section>
+</main>
 @endsection
